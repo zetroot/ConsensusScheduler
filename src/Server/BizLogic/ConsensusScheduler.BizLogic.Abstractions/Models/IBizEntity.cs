8eefc2c96ca09ba22e757fe9a73defdcc84f4d2c
zetroot <zetroot@gmail.com>
2020-05-03 17:35:47 +0300

    Add project files.

---
+++ src/Server/BizLogic/ConsensusScheduler.BizLogic.Abstractions/Models/IBizEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsensusScheduler.BizLogic.Abstractions.Models
+{
+    /// <summary>
+    /// Интерфейс сущность слоя бизнес-логики
+    /// </summary>
+    public interface IBizEntity
+    {
+        /// <summary>
+        /// Идентификатор сущности
+        /// </summary>
+        Guid ID { get; }
+    }
+}
