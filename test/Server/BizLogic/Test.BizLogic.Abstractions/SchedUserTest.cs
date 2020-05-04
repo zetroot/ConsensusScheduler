9cfa0ecff6c06649bf5bdaff4bdece923413cc14
zetroot <zetroot@gmail.com>
2020-05-04 20:47:54 +0300

    scheduler user tests

--- test/Server/BizLogic/Test.BizLogic.Abstractions/SchedUserTest.cs
+++ test/Server/BizLogic/Test.BizLogic.Abstractions/SchedUserTest.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using ConsensusScheduler.BizLogic.Abstractions.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using Xunit;
 
 namespace Test.BizLogic.Abstractions
 {
     [ExcludeFromCodeCoverage]
     public class SchedUserTest
     {
+        [Fact]
+        public void CtorFailsOnNullDisplayName()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SchedUser(Guid.NewGuid(), null));
+        }
+
+        [Fact]
+        public void CtorFailsOnNullOrigin()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SchedUser(null));
+        }
+
+        [Fact]
+        public void CtorActuallySetsProps()
+        {
+            // arrange
+            var id = Guid.NewGuid();
+            var displayName = "displayName";
+
+            // act
+            var user = new SchedUser(id, displayName);
+
+            //assert
+            Assert.Equal(id, user.ID);
+            Assert.Equal(displayName, user.DisplayName);
+        }
     }
 }
