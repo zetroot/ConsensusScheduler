9c5c443f7db4ffb66e522d814fdc110929554372
zetroot <zetroot@gmail.com>
2020-05-11 22:09:12 +0300

    bizlogic registration

---
+++ src/Server/BizLogic/ConsensusScheduler.BizLogic.Aggregates/BizLogicDIHelper.cs
@@ -0,0 +1,21 @@
+﻿using ConsensusScheduler.BizLogic.Abstractions.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsensusScheduler.BizLogic.Aggregates
+{
+    public static class BizLogicDIHelper
+    {
+        public static IServiceCollection UseBizLogic(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.TryAddTransient<IPollCRUDAggregate, PollAggregate>();
+            services.TryAddTransient<ISchedUserCRUDAggregate, SchedUserAggregate>();
+
+            return services;
+        }
+    }
+}
