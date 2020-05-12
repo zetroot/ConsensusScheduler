400d711169dcdb001f43a564782039f8266f1cde
zetroot <zetroot@gmail.com>
2020-05-12 17:43:36 +0300

    Update IBizEntity.cs

--- src/Server/BizLogic/ConsensusScheduler.BizLogic.Abstractions/Interfaces/IBizEntity.cs
+++ src/Server/BizLogic/ConsensusScheduler.BizLogic.Abstractions/Interfaces/IBizEntity.cs
@@ -7,6 +7,7 @@ namespace ConsensusScheduler.BizLogic.Abstractions.Interfaces
     /// <summary>
     /// Интерфейс сущности слоя бизнес-логики
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public interface IBizEntity
     {
         /// <summary>
